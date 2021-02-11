8ff4f8d255817e33866a2ec1006f4898fd04d484
연세대 <ylstimetable@gmail.com>
2021-02-11 11:03:43 +0900

    수정

--- timetabl/settings/base.py
+++ timetabl/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = '%f0@=^#_u+ilb*m-fqog)_^bx#!2)j3#t3iq%jb476!x93l%nv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.34.111.215']
+ALLOWED_HOSTS = []
 
 
 # Application definition

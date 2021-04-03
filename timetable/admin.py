6ade39abbc36be276c8448b9955bb35a59ce86dd
연세대 <ylstimetable@gmail.com>
2021-04-03 15:11:32 +0900

    수정

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ClassD
+from .models import ClassD, ClassM
 
 admin.site.register(ClassD)
+admin.site.register(ClassM)
 # Register your models here.

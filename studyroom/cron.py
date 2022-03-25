100fbe95fdfba0a8093be9304186f9caf537ca5b
연세대 <ylstimetable@gmail.com>
2022-03-25 23:52:09 +0900

    수정

--- studyroom/cron.py
+++ studyroom/cron.py
@@ -2,12 +2,10 @@ from .models import Reserve
 from datetime import datetime, timedelta
 
 def schedule_everyday():
-    real_time = datetime.datetime.now()
-    if real_time.weekday() == 4:
-        if real_time.hour == 23:
-            if real_time.minute == 48:
-                q = Reserve.objects.all()
-                q.delete()
+    real_time = datetime.datetime.now().weekday()
+    if real_time == 5:
+        q = Reserve.objects.all()
+        q.delete()
 
 
 

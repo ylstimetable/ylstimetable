3f46f7689fc66dd6ebda695c3a8e5f5d15c2e12c
연세대 <ylstimetable@gmail.com>
2022-03-26 00:29:16 +0900

    수정

--- timetabl/settings/base.py
+++ timetabl/settings/base.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'timetabl.urls'
 
 CRONJOBS = [
-    ('*/1 * * * *', 'studyroom.cron.schedule_everyday'),
+    ('* 8 * * *', 'studyroom.cron.schedule_everyday'),
     ('*/5 * * * *', 'libraryseat.cron.schedule_every_ten_minutes'),
 ]
 

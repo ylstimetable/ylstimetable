c536f704e9861edc12f025d1c854590d00271175
연세대 <ylstimetable@gmail.com>
2022-03-26 09:31:21 +0900

    수정

--- studyroom/cron.py
+++ studyroom/cron.py
@@ -1,6 +1,9 @@
 from .models import Reserve
 from datetime import datetime, timedelta
 
+
 def schedule_week():
-    q = Reserve.objects.all()
-    q.delete()
+    real_time = datetime.datetime.now().weekday()
+    if real_time == 6:
+        q = Reserve.objects.all()
+        q.delete()

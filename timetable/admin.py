4c9010c0b7138d1a7be109e487e28aadbddf9b98
연세대 <ylstimetable@gmail.com>
2022-06-27 14:23:37 +0900

    수정

--- timetable/admin.py
+++ timetable/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import ClassD, ClassM
 from studyroom.models import Reserve
 
+admin.site.register(ClassD)
 admin.site.register(ClassM)
 admin.site.register(Reserve)
 # Register your models here.

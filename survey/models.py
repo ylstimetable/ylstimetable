d133183aa6481dfe7b9672f4b3b27edd4b9cc561
Hayoun Oh <hayounoh@g.harvard.edu>
2023-02-12 12:49:16 -0500

    Update models.py

--- survey/models.py
+++ survey/models.py
@@ -5,27 +5,51 @@ from CustomUser.models import User
 참고한 코드: https://github.com/Pierre-Sassoulas/django-survey/
 """
 
+def validate_choices(choices):
+    """Verifies that there is at least two choices in choices
+    :param String choices: The string representing the user choices.
+    """
+    values = choices.split(settings.CHOICES_SEPARATOR)
+    empty = 0
+    for value in values:
+        if value.replace(" ", "") == "":
+            empty += 1
+    if len(values) < 2 + empty:
+        msg = "The selected field requires an associated list of choices."
+        msg += " Choices must contain more than one item."
+        raise ValidationError(msg)
+
+        
+        
+"""
+Models starts here!
+"""
+
 # Post를 ForeignKey로 가지는 모델들: Question, Response
 class Post(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
-    create_date = models.DateTimeField()
+    create_date = models.DateField(_("Publication date"), blank=True, null=False, default=now)
     end_date = models.DateTimeField()
     visible = models.BooleanField(max_length=2, null=True)
 
     def __str__(self):
         return self.subject
     
+    
 # Question을 ForeignKey로 가지는 모델들: Answer
 class Question(models.Model): 
+    content = models.TextField(_("Content"))
     subject = models.CharField(max_length=200)
     post = models.ForeignKey(Post, on_delete=models.CASCADE) 
     
     # 선택지는 문자열로 저장되며 필요할때 그때그때 get_choices 활용하여 tuple 형태로 리턴해주면 됩니다. 
     choices = models.TextField(_("Choices"), blank=True, null=True, help_text=CHOICES_HELP_TEXT)
     
-    def __str__(self): 
-        return self.subject
+    
+    def save(self, *args, **kwargs):
+        validate_choices(self.choices)
+        super().save(*args, **kwargs)
     
     # 선택지를 tuple 형태로 리턴해줍니다. 
     def get_choices(self):
@@ -51,13 +75,25 @@ class Question(models.Model):
                 choices_list.append(choice)
         return choices_list
 
+    def __str__(self):
+        msg = f"Question '{self.content}' "
+        msg += f"{self.get_clean_choices()}"
+        return msg
+    
     
 # Response를 ForeignKey로 가지는 모델들: Answer
 class Response(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
     post = models.ForeignKey(Post, null=True, blank=True,
                                  on_delete=models.SET_NULL)
+    
+    created = models.DateTimeField(_("Creation date"), auto_now_add=True)
+
+    def __str__(self):
+        msg = f"Response to {self.post} by {self.user}"
+        msg += f" on {self.created}"
+        return msg
+    
    
 class Answer(models.Model): 
     question = models.ForeignKey(Question, on_delete=models.SET_NULL)

a394682096bbdab0bf9ab2932a2ab37a1478a1cb
Hayoun Oh <hayounoh@g.harvard.edu>
2023-02-12 11:22:47 -0500

    Update models.py

--- survey/models.py
+++ survey/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 from CustomUser.models import User
+
+
+class Post(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    visible = models.BooleanField(max_length=2, null=True)
+
+    def __str__(self):
+        return self.subject
+    
+class Response(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    post = models.ForeignKey(Post, null=True, blank=True,
+                                 on_delete=models.SET_NULL)

c2dfe6c3b9b5d4c4a7ee6dc103b1ccc076b1c640
Hayoun Oh <hayounoh@g.harvard.edu>
2023-02-12 13:41:36 -0500

    Update admin.py

--- survey/admin.py
+++ survey/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Question, Response, Answer
 
-admin.site.register(Post)
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+
+class PostAdmin(admin.ModelAdmin):
+  
+    fieldsets = [
+        (None,               {'fields': ['subject', 'content']}),
+        ('Date information', {'fields': ['create_date', 'end_date', 'visible']}),
+    ]
+    inlines = [QuestionInline]
+
+    
+admin.site.register(Post, PostAdmin)

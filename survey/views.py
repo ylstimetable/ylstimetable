3d31586624201c29677118c452637ce72f9a9d7e
Hayoun Oh <hayounoh@g.harvard.edu>
2023-02-12 15:08:08 -0500

    Update views.py

--- survey/views.py
+++ survey/views.py
@@ -37,5 +37,5 @@ def detail(request, post_id):
     options = []
     for q in questions:
         options.append(q.get_choices())
-    context = {'post': post, 'questions': questions, 'options': options}
+    context = {'post': post, 'qao': zip(questions, options)}
     return render(request, 'survey_detail.html', context)

64e82c5834c86db6379d8f981cc4112d9d54ae45
Hayoun Oh <hayounoh@g.harvard.edu>
2023-02-12 14:32:10 -0500

    Update urls.py

--- survey/urls.py
+++ survey/urls.py
@@ -5,8 +5,7 @@ app_name='survey'
 
 urlpatterns = [
     path('list/', views.list, name='list'),
-    #path('detail/<int:post_id>', views.detail, name='detail'),
-    #path('post/comment/create/<int:post_id>', views.comment_create, name='comment_create'),
-    #path('admin/', views.seat_admin, name='seat_admin')
+    path('detail/<int:post_id>', views.detail, name='detail')
+    #path('post/comment/create/<int:post_id>', views.comment_create, name='comment_create')
 ]
 

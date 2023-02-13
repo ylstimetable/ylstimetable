dcd39e4b675c224de029477b2d55c43c9174dd46
Hayoun Oh <hayounoh@g.harvard.edu>
2023-02-13 10:38:18 -0500

    Update admin.py

--- survey/admin.py
+++ survey/admin.py
@@ -23,15 +23,36 @@ class PostAdmin(admin.ModelAdmin):
     @admin.action(description='Export selected as csv')
     def export_to_csv(self, request, queryset):
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(str(self.model))
-        print('attachment; filename={}.csv'.format(str(self.model)))
-        writer = csv.writer(response)
-
-        for obj in queryset:
-            print(obj)
-            #row = writer.writerow([getattr(obj, field) for field in field_names])
-
+        
+        for i, post in enumerate(queryset):
+            if i == 0:
+                response['Content-Disposition'] = 'attachment; filename={}.csv'.format(str(post))
+                writer = csv.writer(response)
+                header = ["user"]  
+                for question in post.question_set.all():
+                    header.append(question.subject)
+                print(header)
+            else:
+                pass
+                
+       
+        '''
+        return header, question_order
+            else:
+                if settings.EXCEL_COMPATIBLE_CSV:
+                    survey_as_csv = str(survey_as_csv).replace(f"{Survey2Csv.EXCEL_HACK}\n", "")
+                if i != 0:
+                    filename += f"-{survey.safe_name}"
+                elif settings.EXCEL_COMPATIBLE_CSV:
+                    # If we need to be compatible with excel and it's the first survey
+                    response.write(f"{Survey2Csv.EXCEL_HACK}\n")
+                response.write(f"{survey.name}\n")
+                response.write(survey_as_csv)
+                response.write("\n\n")
+        response["Content-Disposition"] = f"attachment; filename={filename}.csv"
+        '''
         return response
+    
 
 
 admin.site.register(Post, PostAdmin)

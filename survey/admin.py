53fe87de612aa847ad04a191b4078c374669c9a6
Hayoun Oh <hayounoh@g.harvard.edu>
2023-02-13 10:56:01 -0500

    Update admin.py

--- survey/admin.py
+++ survey/admin.py
@@ -32,25 +32,15 @@ class PostAdmin(admin.ModelAdmin):
                 for question in post.question_set.all():
                     header.append(question.subject)
                 print(header)
+                writer.writerow(header)
+                
+                for r in post.response_set.all(): 
+                    print(r)
+                    #row = writer.writerow(r)
             else:
                 pass
                 
        
-        '''
-        return header, question_order
-            else:
-                if settings.EXCEL_COMPATIBLE_CSV:
-                    survey_as_csv = str(survey_as_csv).replace(f"{Survey2Csv.EXCEL_HACK}\n", "")
-                if i != 0:
-                    filename += f"-{survey.safe_name}"
-                elif settings.EXCEL_COMPATIBLE_CSV:
-                    # If we need to be compatible with excel and it's the first survey
-                    response.write(f"{Survey2Csv.EXCEL_HACK}\n")
-                response.write(f"{survey.name}\n")
-                response.write(survey_as_csv)
-                response.write("\n\n")
-        response["Content-Disposition"] = f"attachment; filename={filename}.csv"
-        '''
         return response
     
 

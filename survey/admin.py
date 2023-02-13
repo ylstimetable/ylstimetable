df00635c88a5092d750a9fd182a252d6bdfceccb
Hayoun Oh <hayounoh@g.harvard.edu>
2023-02-13 04:42:22 -0500

    Update admin.py

--- survey/admin.py
+++ survey/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from .models import Post, Question, Response, Answer
 
+import csv
+from django.http import HttpResponse
 
-@admin.action(description='Export selected as csv')
-def export_to_csv(self, request, queryset):
-    pass
+
 
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 1
 
+    
 class PostAdmin(admin.ModelAdmin):
   
     fieldsets = [
@@ -18,6 +19,20 @@ class PostAdmin(admin.ModelAdmin):
     ]
     inlines = [QuestionInline]
     actions = [export_to_csv]
+    
+    @admin.action(description='Export selected as csv')
+    def export_to_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(self.model)
+        writer = csv.writer(response)
+
+        #writer.writerow(field_names)
+        for obj in queryset:
+            print([getattr(obj, field) for field in field_names])
+            #row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Response)

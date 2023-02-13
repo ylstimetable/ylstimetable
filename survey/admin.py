42e5aa7864d9c1295517dc2a9b5b5f3cbfa37d45
Hayoun Oh <hayounoh@g.harvard.edu>
2023-02-13 11:18:12 -0500

    Update admin.py

--- survey/admin.py
+++ survey/admin.py
@@ -3,6 +3,7 @@ from .models import Post, Question, Response, Answer
 
 import codecs
 import csv
+import xlwt
 from django.http import HttpResponse
 
 
@@ -19,7 +20,7 @@ class PostAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['end_month', 'end_day', 'visible']}),
     ]
     inlines = [QuestionInline]
-    actions = ['export_to_csv']
+    actions = ['export_to_csv', 'xls_export']
     
     @admin.action(description='Export selected as csv')
     def export_to_csv(self, request, queryset):
@@ -40,6 +41,38 @@ class PostAdmin(admin.ModelAdmin):
                 
        
         return response
+        
+    
+    @admin.action(description='xls로 저장')
+    def xls_export(self, request, queryset):
+        response = HttpResponse(content_type="application/vnd.ms-excel")
+        
+        
+        for i, post in enumerate(queryset):
+            if i == 0:
+                response["Content-Disposition"] = 'attachment;filename*=UTF-8\'\'{}.xls'.format(str(post))
+                wb = xlwt.Workbook(encoding='ansi') #encoding은 ansi로 해준다.
+                
+                ws = wb.add_sheet('답변') #시트 추가
+                
+                ws.write(0, 0, "user")
+                for i, question in enumerate(post.question_set.all()):
+                    ws.write(0, i, question.subject)
+                    
+        return reponse
+    '''
+    
+    #데이터 베이스에서 유저 정보를 불러온다.
+    rows = User.objects.all().values_list('name', 'email') 
+    
+    #유저정보를 한줄씩 작성한다.
+    for row in rows:
+    	row_num +=1
+        for col_num, attr in enumerate(row):
+        	ws.write(row_num, col_num, attr)
+            
+    wb.save(response)
+    '''
     
 
 

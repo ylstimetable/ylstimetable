fa35475a4b6756e6926673e242111e81fa0a15d2
연세대 <ylstimetable@gmail.com>
2023-02-20 00:13:57 +0900

    어드민 css

--- timetabl/settings/base.py
+++ timetabl/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -148,6 +149,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
--- timetabl/settings/prod.py
+++ timetabl/settings/prod.py
@@ -4,8 +4,6 @@ ALLOWED_HOSTS = ['3.36.115.169', 'ylstimetable.com']
 
 STATIC_ROOT = BASE_DIR / 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static/'
-]
+STATICFILES_DIRS = []
 
 DEBUG = False

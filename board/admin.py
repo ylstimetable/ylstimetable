230efc09e13d8198132624782401bd68daea884a
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-04 01:02:48 +0900

    Update admin.py

--- board/admin.py
+++ board/admin.py
@@ -4,6 +4,9 @@ from .models import ClassA, ClassA_Post
 class ClassAAdmin(admin.ModelAdmin):
   pass
   list_display=('subject','professor','semester')
+  list_editable=('semester')
+  list_filter=('subject','professor')
+  search_fields=('subject','professor','semester')
 
 admin.site.register(ClassA, ClassAAdmin)
 admin.site.register(ClassA_Post)

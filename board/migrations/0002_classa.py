a327ff6e5cb0f17f8ffcf310e03405aed379f14c
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-03 22:40:25 +0900

    Update 0002_classa.py

--- board/migrations/0002_classa.py
+++ board/migrations/0002_classa.py
@@ -12,6 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.CreateModel(
             name='ClassA',
+            list_display=('subject','professor'),
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('subject', models.CharField(max_length=200)),

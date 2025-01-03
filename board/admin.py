2fd2cbf4e2e402f03e26ccbef5baf91aa8962ccb
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-04 01:14:37 +0900

    Update admin.py

--- board/admin.py
+++ board/admin.py
@@ -4,7 +4,6 @@ from .models import ClassA, ClassA_Post
 class ClassAAdmin(admin.ModelAdmin):
   pass
   list_display=('subject','professor','semester')
-  list_display_links=('subject','professor')
   # list_editable=('semester')
   list_filter=('subject')
 

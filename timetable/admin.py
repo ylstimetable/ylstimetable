23db5fcd66c1ab7b16b0d67a0537462959a340ee
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-04 01:54:41 +0900

    Update admin.py

--- timetable/admin.py
+++ timetable/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import ClassD, ClassM
 from studyroom.models import Reserve
 
-admin.site.register(ClassD)
+class ClassDAdmin(admin.ModelAdmin):
+  pass
+  list_display=('subject','professor')
+
+admin.site.register(ClassD, ClassDAdmin)
 admin.site.register(ClassM)
 admin.site.register(Reserve)
 # Register your models here.

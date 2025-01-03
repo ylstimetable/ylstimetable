ca629e298e97fa764a1abc277ab1bac245bab261
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-03 21:44:21 +0900

    Update admin.py

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import ClassA, ClassA_Post
 
+class ClassAAdmin(admin.ModelAdmin):
+  pass
+  list_display = ('classname', 'professor')
+
 admin.site.register(ClassA)
 admin.site.register(ClassA_Post)
 # Register your models here.

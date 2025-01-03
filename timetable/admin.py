2afac095a1892f16678694b6fc441b9decc31e7c
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-04 01:55:25 +0900

    Update admin.py

--- timetable/admin.py
+++ timetable/admin.py
@@ -4,7 +4,7 @@ from studyroom.models import Reserve
 
 class ClassDAdmin(admin.ModelAdmin):
   pass
-  list_display=('subject','professor')
+  list_display=('title','professor','time','room')
 
 admin.site.register(ClassD, ClassDAdmin)
 admin.site.register(ClassM)

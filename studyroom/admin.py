cd3d939b5fb26ae380e46ab488d2108780160527
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-07 14:54:07 +0900

    Update admin.py

--- studyroom/admin.py
+++ studyroom/admin.py
@@ -4,14 +4,14 @@ from .models import Reserve
 
 class ReserveAdmin(admin.ModelAdmin):
   pass
-  list_display=('room','room_number')
+  list_display=('room')
   search_fields=('author')
 
-  def room_number('room'):
-    if room=='1':
-      return 'B126'
-    else:
-      return 'no'
+#  def room_number('room'):
+ #   if room=='1':
+  #    return 'B126'
+   # else:
+    #  return 'no'
   
 
 

0310299a8d7239115c020fee09320d903a7039f0
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-07 14:55:28 +0900

    Update admin.py

--- studyroom/admin.py
+++ studyroom/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Reserve
+#from .models import Reserve
 # Register your models here.
 
-class ReserveAdmin(admin.ModelAdmin):
-  pass
-  list_display=('room')
-  search_fields=('author')
+#class ReserveAdmin(admin.ModelAdmin):
+ # pass
+  #list_display=('room')
+#  search_fields=('author')
 
 #  def room_number('room'):
  #   if room=='1':

b8fcd7e500398d5b3426b10c35ddd9ed77c16314
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-08 19:13:47 +0900

    Update admin.py

--- board/admin.py
+++ board/admin.py
@@ -3,10 +3,15 @@ from .models import ClassA, ClassA_Post
 
 class ClassAAdmin(admin.ModelAdmin):
   pass
-  list_display=('subject','professor','semester')
+  list_display=('subject','professor','semester','ox')
   search_fields=('subject','professor')
   ordering=('subject',)
   list_editable=('semester',)
+
+  def ox(self, obj):
+    if self.professor=="이기범":
+      return "o"
+    return "x"
 
 class ClassA_PostAdmin(admin.ModelAdmin):
   pass

494fa24a35e8d082f8364171686919c693fd40cd
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-09 00:22:46 +0900

    Update admin.py

--- timetable/admin.py
+++ timetable/admin.py
@@ -8,6 +8,10 @@ class ClassDAdmin(admin.ModelAdmin):
 
 class ReserveAdmin(admin.ModelAdmin):
   pass
+  list_display=('room','reserved_day')
+  def reserved_day(self, obj):
+    return "{}-{}-{}".format(obj.year,obj.month,obj.day)
+  
 
 admin.site.register(ClassD, ClassDAdmin)
 admin.site.register(ClassM)

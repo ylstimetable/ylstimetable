da14bd655a28c70d17f65f7c48d0bba14c38d051
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-08 19:17:17 +0900

    Update admin.py

--- board/admin.py
+++ board/admin.py
@@ -9,7 +9,7 @@ class ClassAAdmin(admin.ModelAdmin):
   list_editable=('semester',)
 
   def ox(self, obj):
-    if self.professor=="이기범":
+    if professor=="이기범":
       return "o"
     return "x"
 

2110c73ec3f32dc405eb4283e28601796c3f36e8
YeiJin12 <155135768+YeiJin12@users.noreply.github.com>
2025-01-09 00:14:57 +0900

    Update admin.py

--- timetable/admin.py
+++ timetable/admin.py
@@ -8,38 +8,6 @@ class ClassDAdmin(admin.ModelAdmin):
 
 class ReserveAdmin(admin.ModelAdmin):
   pass
-  list_display=('room','room_name','date')
-  def room_name(self, obj):
-    if obj.room=="1":
-      name="B126"
-      return name
-    elif obj.room=="2":
-      name="B127"
-      return name
-    elif obj.room=="3":
-      name="B112"
-      return name
-    elif obj.room=="4":
-      name="B113"
-      return name
-    elif obj.room=="5":
-      name="B114"
-      return name
-    elif obj.room=="6":
-      name="B116"
-      return name
-    elif obj.room=="7":
-      name="212C"
-      return name
-    elif obj.room=="8":
-      name="212D"
-      return name
-    elif obj.room="9":
-      name="212E"
-      return name
-    elif obj.room="10":
-      name="212F"
-      return name
 
 admin.site.register(ClassD, ClassDAdmin)
 admin.site.register(ClassM)
